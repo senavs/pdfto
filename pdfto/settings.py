60658da8d4b70d8fc825314c1adecd76d0af548d
Matheus Sena Vasconcelos <sena.matheus14@gmail.com>
2019-12-30 14:20:31 -0300

    create project architecture

---
+++ pdfto/settings.py
@@ -0,0 +1,25 @@
+import os
+
+# flask settings
+FLASK_APP = os.environ.get('FLASK_APP', 'PDFto')
+FLASK_ENVIRONMENT = os.environ.get('FLASK_ENVIRONMENT', 'development')
+FLASK_SERVER_NAME = os.environ.get('FLASK_SERVER_NAME', None)
+FLASK_HOST = os.environ.get('FLASK_HOST', '0.0.0.0')
+FLASK_PORT = os.environ.get('FLASK_PORT', '5000')
+FLASK_DEBUG = os.environ.get('FLASK_DEBUG', True)
+FLASK_RESTPLUS_VALIDATE = os.environ.get('FLASK_RESTPLUS_VALIDATE', True)
+FLASK_RESTPLUS_MASK_SWAGGER = os.environ.get('FLASK_RESTPLUS_MASK_SWAGGER', False)
+
+# endpoints
+ENDPOINT_1 = 'endpoint'
+ENDPOINT_1_DESCRIPTION = 'description endpoint 1'
+ENDPOINT_1_ROUTE_1 = '/route1'
+
+# swagger settings
+SWAGGER_DOC = '/swagger'
+SWAGGER_TITLE = 'PDFto'
+SWAGGER_DESCRIPTION = 'A Python Flask API to manage PDF files.'
+SWAGGER_VERSION = '1.0'
+SWAGGER_CONTACT = 'Matheus Sena Vasconcelos'
+SWAGGER_CONTACT_URL = 'https://linkedin.com/in/senavs'
+SWAGGER_CONTACT_EMAIL = 'sena.matheus14@gmail.com'
