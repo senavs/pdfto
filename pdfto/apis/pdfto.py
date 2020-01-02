2ce3d6c52aa82f5cb2bcd2ce129737e68221ef42
Matheus Sena Vasconcelos <sena.matheus14@gmail.com>
2020-01-02 15:53:19 -0300

    add /totext route to settings and create /totext route

--- pdfto/apis/pdfto.py
+++ pdfto/apis/pdfto.py
@@ -18,7 +18,7 @@ NPAGE_OUTPUT = api.model('npage output', {'page': fields.Integer(required=True,
                                                                  description='Number of pages in PDF',
                                                                  example=9),
                                           'error': fields.String(required=True,
-                                                                 description='If any error occured',
+                                                                 description='If any error occurred',
                                                                  example='File not found or file is not a PDF')})
 
 
@@ -30,12 +30,44 @@ class NPage(Resource):
     @api.marshal_with(NPAGE_OUTPUT)
     def post(self):
         input_request = request.json
-        output_request = {'page': -1, 'error': ''}
+        output_request = {'page': None, 'error': ''}
 
         with TemporaryBase64(settings.ENDPOINT_PDFTO_B64_DIRECTORY) as file:
             try:
                 file.write_string(input_request['file'])
                 output_request['page'] = mypdf.npage(file.file_path)
+            except (BrokenBase64, MyPDFFileError) as err:
+                output_request['error'] = err.message
+                return output_request
+        return output_request
+
+
+TOTEXT_INPUT = api.model('totext input', {'file': fields.String(required=True,
+                                                                description='File as base64',
+                                                                example='TWF0aGV1cyBTZW5hIFZhc2NvbmNlbG9z')})
+
+TOTEXT_OUTPUT = api.model('totext output', {'text': fields.String(required=True,
+                                                                  description='Text extract from PDF',
+                                                                  example='This is the text extracted from PDF file'),
+                                            'error': fields.String(required=True,
+                                                                   description='If any error occurred',
+                                                                   example='File not found or file is not a PDF')})
+
+
+@api.route(settings.ENDPOINT_PDFTO_ROUTE_TOTEXT)
+class ToText(Resource):
+
+    @api.response(http_code.SUCCESS_200.code, http_code.SUCCESS_200.description)
+    @api.expect(TOTEXT_INPUT)
+    @api.marshal_with(TOTEXT_OUTPUT)
+    def post(self):
+        input_request = request.json
+        output_request = {'text': '', 'error': ''}
+
+        with TemporaryBase64(settings.ENDPOINT_PDFTO_B64_DIRECTORY) as file:
+            try:
+                file.write_string(input_request['file'])
+                output_request['text'] = mypdf.totext(file.file_path)
             except (BrokenBase64, MyPDFFileError) as err:
                 output_request['error'] = err.message
                 return output_request
--- pdfto/settings.py
+++ pdfto/settings.py
@@ -19,6 +19,7 @@ ENDPOINT_1_ROUTE_1 = '/route1'
 ENDPOINT_PDFTO = 'pdfto'
 ENDPOINT_PDFTO_DESCRIPTION = 'Manage PDF files'
 ENDPOINT_PDFTO_ROUTE_NPAGE = '/npage'
+ENDPOINT_PDFTO_ROUTE_TOTEXT = '/totext'
 ENDPOINT_PDFTO_B64_DIRECTORY = '/home/sena/Documents'
 
 # swagger settings
